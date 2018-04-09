0f3e1b45a8dfacc7ebf925e9c72c067637275fd4
d0iasm <doiasami1219@gmail.com>
2018-04-09 22:17:00 +0900

    Read 18 sectors

--- helloos.asm
+++ helloos.asm
@@ -6,7 +6,7 @@
   DB 0x90
 
   DB "HELLOIPL"
-  DW 512
+  DW 512 ; Size of a sector
   DB 1
   DW 1
   DB 2
@@ -20,8 +20,8 @@
   DD 2880
   DB 0, 0, 0x29
   DD 0xffffffff
-  DB "HELLO-OS   "
-  DB "FAT12   "
+  DB "HELLO-OS   " ; Disk name 11 byte
+  DB "FAT12   " ; Format name 8 byte
   RESB 18
 
 ; Main program
@@ -32,6 +32,16 @@ entry:
   MOV DS, AX
   MOV ES, AX
   MOV SI, msg
+
+  ; Load disk
+  MOV AX, 0x0820
+  MOV ES, AX
+  MOV CH, 0
+  MOV DH, 0
+  MOV CL, 2
+
+readloop:
+  MOV SI, 0
 
 putloop:
   MOV AL, [SI]
@@ -57,8 +67,32 @@ msg:
 
   DB 0x55, 0xaa
 
-  DB 0xf0, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00
-  RESB 4600
-  DB 0xf0, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00
-  RESB 1469432
+  ; DB 0xf0, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00
+  ; RESB 4600
+  ; DB 0xf0, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00
+  ; RESB 1469432
+
+retry:
+  MOV AH, 0x02 ; Failing count
+  MOV AL, 1
+  MOV EX, 0
+  MOV DL, 0x00
+  INT 0x13 ; Call disk BIOS
+  JNC next ; Move next if error isn't happened
+  ADD SI, 1
+  CMP SI, 5 ; Compare SI and 5
+  JAE error ; Move error if SI >= 5
+  MOV AH, 0x00
+  MOV DL, 0x00
+  INT 0x13
+  JMP retry
+
+next:
+  MOV AX, ES ; Move 0x200 addresses
+  ADD AX, 0x0020
+  MOV ES, AX
+  ADD CL, 1
+  CMP CL, 18
+  JBE readloop ; Move readloop if CL <= 18
+
 
