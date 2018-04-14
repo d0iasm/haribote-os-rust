fb4acc72830b839d04f8d3cb8bcfa6ef2a0497cc
d0iasm <doiasami1219@gmail.com>
2018-04-14 23:32:08 +0900

    Error linking with  failed

--- src/bootpack.rs
+++ src/bootpack.rs
@@ -1,7 +1,11 @@
 #![feature(asm)]
+#![feature(lang_items)]
 #![no_main]
+#![no_std]
 
-// #[no_mangle]
+#[lang = "eh_personality"] extern fn eh_personality() {}
+#[lang = "panic_fmt"] extern fn panic_fmt() -> ! { loop {} }
+
 #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
 fn hlt() {
     unsafe {
@@ -16,3 +20,4 @@ pub fn init_os() {
         hlt();
     }
 }
+
